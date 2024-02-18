7253deca12549b05c51d70d2294b943b86581243
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-02-18 14:24:43 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,5 @@
+// 5 .Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// . He said, “I am quite sensible of the difficulties that lie in our way, all that I can say is that if we cannot get over our difficulties, the world will soon get rid of us. ”
+
+let famous_person : string="ALLAMA IQBAL Said"
+console.log(`${famous_person}, I am quite sensible of the difficulties that lie in our way, all that I can say is that if we cannot get over our difficulties, the world will soon get rid of us.`)
