e6e3a3b7a6057df8f0077f23aae643d2962ad263
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 00:08:58 +0500

    Add files via upload

---
+++ Qno5.js
@@ -0,0 +1,7 @@
+"use strict";
+// 5 .Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+// . He said, “I am quite sensible of the difficulties that lie in our way, all that I can say is that if we cannot get over our difficulties, the world will soon get rid of us. ”
+Object.defineProperty(exports, "__esModule", { value: true });
+let famous_person = "ALLAMA IQBAL";
+let message = "“I am quite sensible of the difficulties that lie in our way, all that I can say is that if we cannot get over our difficulties, the world will soon get rid of us. ”";
+console.log(`${famous_person}. He said ${message}`);
