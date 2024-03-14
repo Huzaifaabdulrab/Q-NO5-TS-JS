e18bd1d011f2fe6b3a390aa430c21f6112d0bd9a
Huzaifaabdulrab <149306467+Huzaifaabdulrab@users.noreply.github.com>
2024-03-14 05:21:01 +0500

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-// 5 .Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
-// . He said, “I am quite sensible of the difficulties that lie in our way, all that I can say is that if we cannot get over our difficulties, the world will soon get rid of us. ”
-var famous_person = "ALLAMA IQBAL Said";
-console.log("".concat(famous_person, ", I am quite sensible of the difficulties that lie in our way, all that I can say is that if we cannot get over our difficulties, the world will soon get rid of us."));
